e9ee1fd1c8681e40c4a005b4f31db44b49f8e346
GuilleK <kondra.guille@gmail.com>
2023-08-05 18:25:50 -0300

    tres

---
+++ random01/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './app.css' //importar siempre para visualizar. 
+
+const App = () => {
+
+
+  return (
+    <div>
+    <h1>HOLA DESDE REACT</h1>
+    </div>
+  )
+}
+
+export default App
